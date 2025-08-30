6d901445f720af0925391aabea0769dcea27c9a6
MonikaMohapatra <monikagudi123@gmail.com>
2025-08-30 12:54:53 +0530

    new c

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
